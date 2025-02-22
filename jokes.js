e187d70aed2ce863f58550eb55ef8d5cc3ff9534
yashsainani <yashsainani1803@gmail.com>
2025-02-22 22:47:12 +0530

    publishing all required files

---
+++ jokes.js
@@ -0,0 +1,194 @@
+module.exports = [
+  {
+    joke: "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+    type: "one-liner",
+  },
+  {
+    joke: "Why don’t skeletons fight each other? They don’t have the guts.",
+    type: "pun",
+  },
+  {
+    joke: "I used to play piano by ear, but now I play it with my hands.",
+    type: "pun",
+  },
+  {
+    joke: "Why don't graveyards have 4G? Because they're full of dead spots.",
+    type: "dark",
+  },
+  {
+    joke: "I have a fear of speed bumps, but I’m slowly getting over it.",
+    type: "pun",
+  },
+  {
+    joke: "I would tell you a chemistry joke, but I know I wouldn't get a reaction.",
+    type: "pun",
+  },
+  {
+    joke: "My therapist says I have a preoccupation with vengeance. We’ll see about that.",
+    type: "dark",
+  },
+  {
+    joke: "I used to play piano by ear, but now I play it with my hands.",
+    type: "pun",
+  },
+  {
+    joke: "I have an emotional support skeleton. He's always there for me.",
+    type: "dark",
+  },
+  {
+    joke: "I’m reading a book on anti-gravity. It’s impossible to put down.",
+    type: "pun",
+  },
+  {
+    joke: "I’m on a whiskey diet. I’ve lost three days already.",
+    type: "one-liner",
+  },
+  {
+    joke: "I told my friend 10 jokes to make him laugh. Sadly, no pun in ten did.",
+    type: "pun",
+  },
+  {
+    joke: "I have a lot of jokes about unemployed people, but none of them work.",
+    type: "dark",
+  },
+  {
+    joke: "I’d explain a joke about a pencil, but it’s pointless.",
+    type: "pun",
+  },
+  {
+    joke: "I’m not arguing, I’m just explaining why I’m right.",
+    type: "one-liner",
+  },
+  {
+    joke: "I asked the librarian if the library had any books on suicide. She said they're all checked out.",
+    type: "dark",
+  },
+  {
+    joke: "I wonder why the baseball kept getting bigger. Then it hit me.",
+    type: "pun",
+  },
+  {
+    joke: "I have a joke about a broken pencil, but it's pointless.",
+    type: "pun",
+  },
+  {
+    joke: "I’m writing a book on reverse psychology. Don’t buy it.",
+    type: "pun",
+  },
+  {
+    joke: "I asked the waiter if the restaurant served fish. He said, 'No, we serve people.'",
+    type: "dark",
+  },
+  {
+    joke: "I used to be a baker, but I couldn’t make enough dough.",
+    type: "pun",
+  },
+  {
+    joke: "I told my computer I needed a break, and now it won’t stop sending me ads for vacations.",
+    type: "pun",
+  },
+  {
+    joke: "I’m on a seafood diet. I see food, and I eat it.",
+    type: "one-liner",
+  },
+  {
+    joke: "I’m trying to lose weight but it’s not working. Maybe I should try losing some friends first.",
+    type: "dark",
+  },
+  {
+    joke: "I can’t believe I got fired from the calendar factory. All I did was take a day off.",
+    type: "pun",
+  },
+  {
+    joke: "I told my wife she should embrace her mistakes. She gave me a hug.",
+    type: "one-liner",
+  },
+  {
+    joke: "I was going to tell you a time-travel joke, but you didn’t like it.",
+    type: "pun",
+  },
+  {
+    joke: "I’m really good at my job, but I’m not sure how long I can keep up the pretending.",
+    type: "dark",
+  },
+  {
+    joke: "I don’t trust stairs. They’re always up to something.",
+    type: "pun",
+  },
+  {
+    joke: "I’d explain a joke about an elevator, but it’s an uplifting experience.",
+    type: "pun",
+  },
+  {
+    joke: "I know they say that money talks, but all mine says is 'Goodbye.'",
+    type: "one-liner",
+  },
+  {
+    joke: "I’m really bad at math, but I know that two wrongs don’t make a right… but three rights make a left.",
+    type: "pun",
+  },
+  {
+    joke: "I’m not lazy. I’m on energy-saving mode.",
+    type: "one-liner",
+  },
+  {
+    joke: "I have a joke about construction, but I’m still working on it.",
+    type: "pun",
+  },
+  {
+    joke: "I asked my dog what’s two minus two. He said nothing.",
+    type: "one-liner",
+  },
+  {
+    joke: "I’m trying to lose weight, but it’s not working. Maybe I should try losing some friends first.",
+    type: "dark",
+  },
+  {
+    joke: "I don’t suffer from insanity; I enjoy every minute of it.",
+    type: "one-liner",
+  },
+  {
+    joke: "What’s orange and sounds like a parrot? A carrot.",
+    type: "pun",
+  },
+  {
+    joke: "I used to be indecisive, but now I’m not so sure.",
+    type: "one-liner",
+  },
+  {
+    joke: "The other day, I bought a belt the size of a baby, and now I’m a father of two.",
+    type: "dark",
+  },
+  {
+    joke: "I’m on a whiskey diet. I’ve lost three days already.",
+    type: "one-liner",
+  },
+  {
+    joke: "I have a joke about an elevator, but it’s an uplifting experience.",
+    type: "pun",
+  },
+  {
+    joke: "I was going to tell you a joke about an elevator, but it’s an uplifting experience.",
+    type: "pun",
+  },
+  {
+    joke: "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+    type: "one-liner",
+  },
+  {
+    joke: "I told my wife she should embrace her mistakes. She gave me a hug.",
+    type: "one-liner",
+  },
+  {
+    joke: "I couldn’t figure out why I was walking in circles. Then I realized I was lost.",
+    type: "dark",
+  },
+  {
+    joke: "I’m really good at my job, but I’m not sure how long I can keep up the pretending.",
+    type: "dark",
+  },
+  {
+    joke: "I have a fear of speed bumps, but I’m slowly getting over it.",
+    type: "pun",
+  },
+];
